814050e76a958a3268a6b180a0b5f769475a4a8c
Mona <hammondmona004@gmail.com>
2025-09-24 10:59:20 -0700

    music ads

--- pages/about.py
+++ pages/about.py
@@ -4,7 +4,7 @@ from components.talent_card import show_talent_card
 from components.subscription import show_subscription
 from components.footer import show_footer
 from components.advert_card import show_advert_card
-import requests 
+import requests
 from utils.api import base_url
 
 @ui.page("/about")
@@ -25,27 +25,28 @@ def show_about_page():
                         "text-7xl text-green-500"
                     )
                 ui.label("Here You Can Prove Your Talent").classes("text-3xl")
-                ui.label(
-                    "Etiam nec imperdiet nisi. Nullam in turpis placerat, elementum felis vitae, varius erat. Sed eleifend, metus ac convallis tincidunt, lorem turpis pharetra nibh, id blandit mi lacus ."
-                ).style("line-height: 2em;").classes('text-lg')
-                with ui.row().classes("flex flex-row justify-center items-center"):
-                    ui.button(
-                        text="Apply Now", on_click=lambda: ui.navigate.to("/about")
-                    ).classes(
-                        "mt-4 px-6 py-3 rounded-full text-white shadow-sm shadow-green-100"
-                    ).props(
-                        "flat dense no-caps"
-                    ).style(
-                        "border: solid 2px green"
-                    )
-                    ui.label("or").classes('text-lg')
-                    ui.button(
-                        text="Find the Talent", on_click=lambda: ui.navigate.to("/")
-                    ).classes(
-                        "mt-4 px-6 py-3 rounded-full text-white bg-green shadow-sm shadow-green-100"
-                    ).props(
-                        "flat dense no-caps"
-                    )
+    
+    with ui.element('section').classes('w-full h-screen flex flex-col justify-center items-center bg-[url("/assets/bg-smoke.png")] bg-cover bg-center text-white px-20'):
+        ui.label("Established in 2013, Tolkit is a global educational media platform for music industry professionals, primarily artist managers and professionally self-managed artists.We help artists & managers make their dreams come true by providing business tools, training, and mentorship from GRAMMY-level experts so that they can become high-demand music industry entrepreneurs.Each year we connect thousands of people from around the globe with the music industry’s top experts through live mentorship, training, interviews, downloads, and online courses.Our experts work in all areas of the industry from major label A&R to tour managers with long lists of superstar artists on their client rosters including everyone from The Rolling Stones to Dolly Parton to Beyonce.").style("line-height: 2em;").classes('text-lg')
+        with ui.row().classes("flex flex-row justify-center items-center"):
+            ui.button(
+                text="Apply Now", on_click=lambda: ui.navigate.to("/about")
+            ).classes(
+                "mt-4 px-6 py-3 rounded-full text-white shadow-sm shadow-green-100"
+            ).props(
+                "flat dense no-caps"
+            ).style(
+                "border: solid 2px green"
+            )
+            ui.label("or").classes('text-lg')
+            ui.button(
+                text="Find the Talent", on_click=lambda: ui.navigate.to("/")
+            ).classes(
+                "mt-4 px-6 py-3 rounded-full text-white bg-green shadow-sm shadow-green-100"
+            ).props(
+                "flat dense no-caps"
+            )
+
 
     with ui.element('section').classes('w-full h-full flex flex-col justify-center items-center py-10 px-10 bg-gray-100 mb-10').style('font-family: "Josefin Sans", sans-serif'):
         with ui.column().classes('flex flex-col justify-center items-center'):
@@ -81,7 +82,7 @@ def show_about_page():
                     "flat dense no-caps"
                 ).style("border: solid 2px green").classes('bg-white text-green px-2 py-2')
 
-    with ui.element("section").classes('w-full h-full text-gray-700 flex flex-col justify-center items-center px-10 py-10').style('font-family: "Josefin Sans", sans-serif'):
+    with ui.element("section").classes('w-full h-screen text-gray-700 flex flex-col justify-center items-center px-10 py-10').style('font-family: "Josefin Sans", sans-serif'):
         with ui.column().classes('flex flex-col justify-center items-center'):
             ui.label("Recently Registered Talents").classes('text-4xl')
             ui.separator().classes('h-1 bg-green w-1/3 mb-8')
